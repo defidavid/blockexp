c259febe3cee04657db2de058bdd22dc9d4daf33
David Salgado <davidsalgado@davids-mbp.attlocal.net>
2020-07-10 10:39:13 -0500

    Added project id and pinned material-ui version

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Provider } from 'react-redux';
 
 
 // Add your token here to avoid throttling by infura
-const INFURA_TOKEN = '';
+const PROJECT_ID = 'e1b4f6d9e6574eff98fa7c39fe5feeb5';
 
-window.web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/${INFURA_TOKEN}`));
+window.web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${PROJECT_ID}`));
 
 ReactDOM.render(
     <Provider store={store}>
